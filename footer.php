3e965d8f3dba96a9e95a939300f67f6f9496b0fd
tofra <tofraweb@gmail.com>
2017-05-14 14:35:54 +0300

    some changes

--- footer.php
+++ footer.php
@@ -4,8 +4,26 @@
         <p>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?></p>
       </footer>
     </div> <!-- /container -->
+    <div class="modal fade" id="contactForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">Contact Us</h4>
+          </div>
+          <div class="modal-body">
+            <?php if( function_exists( 'ninja_forms_display_form' ) ){ ninja_forms_display_form( 1 ); }
+            ?>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 
     <?php wp_footer(); ?>
+
 
   </body>
 </html>
